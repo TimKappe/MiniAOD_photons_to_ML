7d99df2a158a8f6603ec8fe39ec0e877e0473fdf
Tim Kappe <tim.kappe@rwth-aachen.de>
2024-08-13 12:59:10 +0200

    added option to exclude previously processed files, to skip and to shuffle

--- futuresRunner.py
+++ futuresRunner.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 import argparse
+import os
 
 from preprocess_futures import process_file
 
@@ -11,19 +12,43 @@ def process_args() -> Tuple[str, int]:
                                      prog='futureRunner.py')
     parser.add_argument('file', help='file containing the names of the dataset files')
     parser.add_argument('workers', help='set number of cores, (set to 24 on lxblade)')
+    parser.add_argument('--exclude', help='exclude files in this directory')
+    parser.add_argument('--skip', help='skip the first n files (after ignore is applied)')
+    parser.add_argument('--shuffle', action='store_true', help='shuffle input file list')
 
     args = parser.parse_args()
     print(args)
     file_: str = args.file
     workers_: int = int(args.workers)
-    return file_, workers_
+    ignore_dir: str = args.exclude
+
+    files_ = read_filenames(file_)
+
+    if ignore_dir is not None:
+        ignore_list = os.listdir(ignore_dir)
+        for name in ignore_list:
+            name = name.split('.')[0]
+            for i, filename in enumerate(files_):
+                if filename.split('.')[0].split('/')[-1] == name: 
+                    files_.pop(i)
+                    break
+
+    skip = args.skip
+    if skip is not None:
+        for i in range(int(skip)):
+            files_.pop(0)
+    if args.shuffle:
+        import random
+        random.shuffle(files_)
+        print('list shuffled')
+    print(f'INFO: starting to process {len(files_)} files')
+    return files_, workers_
 
 def read_filenames(file_path):
     with open(file_path, 'r') as f:
         return [line.strip() for line in f]
 
-def main(filenames_path, workers=4):
-    filenames = read_filenames(filenames_path)#[:8]
+def main(filenames, workers=4):
     total_files = len(filenames)
     completed = 0  # Initialize a counter for completed tasks
 
@@ -41,5 +66,5 @@ def main(filenames_path, workers=4):
 
 
 if __name__ == '__main__':
-    filenames_path, workers = process_args()
-    main(filenames_path, workers=workers)
+    filenames, workers = process_args()
+    main(filenames, workers=workers)
